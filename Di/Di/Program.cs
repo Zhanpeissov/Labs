d26efac2b2e57a0c478a6f2bb432b6ddb9d8b7dc
Zhanpeissov <zhanpeissov7@gmail.com>
2017-03-09 03:55:19 +0600

    Add files via upload

---
+++ Di/Di/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Di
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo(Console.ReadLine());
+            List<FileSystemInfo> fi = new List<FileSystemInfo>();
+            fi.AddRange(di.GetFiles());
+            foreach (FileInfo p in fi) {
+                StreamReader sr = new StreamReader(p.FullName);
+                string str = sr.ReadToEnd();
+                string[] arr = str.Split();
+                for (int i = 0; i < arr.Length; i++) {
+                    if (arr[i] == "abcd") {
+                        Console.WriteLine(p.Name);
+                    }
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
